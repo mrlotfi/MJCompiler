bfa82808d41aac4ad38799f541b5153c48e44f3f
mrlotfi <mohamadmlnnamin@gmail.com>
2016-01-25 23:37:03 +0330

    second chance

--- src/Compiler/Scanner.java
+++ src/Compiler/Scanner.java
@@ -1,9 +1,23 @@
 package Compiler;
 
-/**
- * Created by mrlotfi on 1/17/16.
- */
+
 public class Scanner {
+
+    public static String[] KEYWORDS_RAW = { "public", "class", "static", "void", "main", "extends",
+            "return", "int", "boolean", "if", "else","while", "switch", "case", "break",
+            "true", "false"};
+
+    private static String IDENTIFIER_PATTERN = "[a-zA-Z][a-zA-Z0-9]*";
+    private static String INTEGER_PATTERN = "[+|-]?\\d+";
+
+    public static void main(String[] args) {
+        System.out.println("8".matches(IDENTIFIER_PATTERN));
+        System.out.println("98".matches(INTEGER_PATTERN));
+    }
+
+
+
+
     public Scanner(String fileName) {
 
     }
